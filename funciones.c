fe970ae758af0b5fca8aad022a9495b9aac56558
JavierJesus <jaanrosa@espol.edu.ec>
2019-06-15 03:18:14 -0500

    Mas agregacion de detalles

--- funciones.c
+++ funciones.c
@@ -3,7 +3,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include <Windows.h>
 
+void Color(int Background, int Text);   
+
+enum Colors { // Listado de colores (La letra "L" al inicio, indica que es un color más claro que su antecesor).
+ BLACK = 0,
+ BLUE = 1,
+ GREEN = 2,
+ CYAN = 3,
+ RED = 4,
+ MAGENTA = 5,
+ BROWN = 6,
+ LGREY = 7,
+ DGREY = 8,
+ LBLUE = 9,
+ LGREEN = 10,
+ LCYAN = 11,
+ LRED = 12,
+ LMAGENTA = 13,
+ YELLOW = 14,
+ WHITE = 15
+};
 int tablero[10][10];
 int minas_x[10];
 int minas_y[10];
@@ -32,7 +53,17 @@ void mostrar_tablero(){
             printf("   0 1 2 3 4 5 6 7 8 9 \n");
         printf("%i  ",i);
         for (int j = 0; j < 10; j++){
+            if(tablero[i][j]==48) Color(BLACK, YELLOW);
+            if(tablero[i][j]==49) Color(BLACK, LGREEN);
+            if(tablero[i][j]==50) Color(BLACK, LBLUE);
+            if(tablero[i][j]==51) Color(BLACK, CYAN);
+            if(tablero[i][j]==52) Color(BLACK, LCYAN);
+            if(tablero[i][j]==53) Color(BLACK, LMAGENTA);
+            if(tablero[i][j]==54) Color(BLACK, LGREY);
+            if(tablero[i][j]==55) Color(BLACK, BLUE);
+            if(tablero[i][j]==157) Color(BLACK, LRED);
             printf("%c ",tablero[i][j]);
+            Color(BLACK, WHITE);
         }
         printf("\n"); 
     }      
@@ -107,4 +138,11 @@ bool comprobar_tablero(){
     }
     return true;
     
+}
+
+//Codigo para cambio de Colores, Autor: Thiamatt
+void Color(int Background, int Text){ 
+ HANDLE Console = GetStdHandle(STD_OUTPUT_HANDLE);
+ int    New_Color= Text + (Background * 16); 
+ SetConsoleTextAttribute(Console, New_Color);
 }
